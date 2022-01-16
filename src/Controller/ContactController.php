d69ebc778b3ef5eb34bcbbf60d5e85bb6ab03f82
Grégory Vandevraye <gvandevray@numericable.fr>
2022-01-16 21:17:34 +0100

    ajout form contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Mail;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/nous-contacter', name: 'contact')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->addFlash('notice','Merci de nous avoir contacté. Notre équipe va vous répondre dans les meilleurs délais.');
+            // envoi de mail
+        }
+        return $this->render('contact/index.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+}
